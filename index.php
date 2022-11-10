27f880b9e62f2d97622b224cddf66dd615db2ce2
Gakii72 <ggdoreen22@gmail.com>
2022-11-10 13:30:30 +0300

    php-getting-started

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+echo "Hello world program" ;
+
+$myName = "Doreen Gakii" ;
+echo $myName;
+var_dump($myName);
+
+// integers
+$a = 30;
+$b="30";
+$c= true;
+$d =200.34;
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+
+echo "my name is ".$myName." .my M-pesa balance is ksh." .$d;
+
+
+
+?>
